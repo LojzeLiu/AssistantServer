f3998987415d142570f4e5fc5e3c557ac5fe6eb5
Lojze SHOW <lojze@foxmail.com>
2018-12-05 19:01:36 +0800

    服务初始化完成

--- WebServer.go
+++ WebServer.go
@@ -3,7 +3,9 @@ package main
 import (
 	"Common"
 	"flag"
+	"fmt"
 	"net/https"
+	"strconv"
 )
 
 var ConfFile = flag.String("f", "./WevServer.conf", "configer file")
@@ -12,6 +14,29 @@ func main() {
 	//解析配置
 	conf := &Common.Configer{}
 	if err := conf.Init(ConfFile); err != nil {
-
+		fmt.Println("Configer initialization Error:", err)
+		return
 	}
+	defer conf.Destroy()
+
+	//获取日志配置
+	logConfs, err := conf.GetConf("LOG")
+	if err != nil {
+		fmt.Println("Get log configuer Error:", err)
+		return
+	}
+	DebugLevel, err := strconv.Atoi(logConfs["Level"])
+	if err != nil {
+		fmt.Println("Log level Error:", err)
+		return
+	}
+
+	//初始化日志
+	if err := Common.SetLogger(logConfs["Path"], logConfs["AppName"], DebugLevel); err != nil {
+		fmt.Println("Set logger Error:", err)
+		return
+	}
+	defer Common.CloseLogger()
+
+	//启动Web Server
 }
