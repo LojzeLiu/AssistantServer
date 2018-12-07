b2dff0d593d1593ef2334ec6af44acb3da686991
LojzeLiu <lojze@foxmail.com>
2018-12-07 15:58:38 +0800

    增加显式数据包

---
+++ DisplyPck/DisplyData.go
@@ -0,0 +1,21 @@
+package DisplyPck
+
+import (
+	"AssistantServer/NetLayer"
+	"Common"
+)
+
+var gCrawler NetLayer.WeatherCrawler
+
+type DisplyData struct {
+	mConf *Common.Configer
+}
+
+func (this *DisplyData) Init(conf *Common.Configer) error {
+	this.mConf = conf
+	return gCrawler.Init(conf) //初始天气爬虫
+}
+
+type ErrorInfo struct {
+	Msg string
+}
